0beccb3f4d50f2b16df1c507a0084c1fa699264e
phantombeast <rajpalpulkit12@gmail.com>
2018-07-25 03:46:02 +0530

    card set up

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Card=(props)=>{
+    const {name,email,id}=props;
+    return(
+        <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="Robots" src={`https://robohash.org/${id}`}/>
+                <div >
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+        </div>
+
+    );
+
+}
+
+export default Card;

6190c8c35d8a9ed2869aa267eedae53e78189ef4
phantombeast <rajpalpulkit12@gmail.com>
2018-07-25 04:18:09 +0530

    cardlist

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {robots} from './robots'
 import 'tachyons'
+import CardList from'./CardList'
 
-import Card from './Card';
+
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-    </div>
+ReactDOM.render(<CardList robots={robots}/>
     , document.getElementById('root'));
 registerServiceWorker();
+

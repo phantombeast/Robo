7dda014934544537786f23de3e0afcf20286e4ca
phantombeast <rajpalpulkit12@gmail.com>
2018-07-26 00:03:55 +0530

    cardlist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {robots} from './robots'
 import 'tachyons'
+import App from './App'
 import CardList from'./CardList'
 
 
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<CardList robots={robots}/>
+ReactDOM.render(<App/>
     , document.getElementById('root'));
 registerServiceWorker();
 
